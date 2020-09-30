ad38daf4f9b620881d5613beedfbc1f697f4f6d9
HarshaanNiles010 <harshaanniles010@gmail.com>
2020-09-30 15:00:31 -0600

    Basic functionalities Accomplished

--- shell379.cpp
+++ shell379.cpp
@@ -28,16 +28,22 @@ int resumeJobs(int pid){
     std::cout << "Pid recieved is: " << pid;
     return 0;
 }
+int suspendJobs(int pid){
+    std::cout << "Pid recieved is: " << pid;
+    return 0;
+}
+int waitJobs(int pid){
+    std::cout << "Pid recieved is: " << pid;
+    return 0;
+}
 int main(int argc, char *argv[]){
     std::string UserInput;
     std::cout << "Shell379: " ;
     int pid = 0;
     std::cin >> UserInput >> pid;
     if(UserInput == "jobs"){ getjobs();}
-    else if(UserInput == "kill"){
+    else if(UserInput == "kill" && pid != 0){
         int killpid = 0;
-        //std::cout << "Enter job pid: "; 
-        //std::cin >> pid; 
         killpid = killJobs(pid);
         if(killpid == 0){
             std::cout << "\nJob killed" << std::endl;
@@ -46,11 +52,38 @@ int main(int argc, char *argv[]){
             std::cout << "Failed to kill the jobs" << std::endl;
         }
     }
-    else if(UserInput == "resume"){ 
+    else if(UserInput == "resume" && pid != 0){ 
         int resumepid = 0;
-        //std::cout <<"enter job pid: ";
-        //std::cin >>  pid;
-        resumeJobs(pid);
+        resumepid = resumeJobs(pid);
+        if(resumepid == 0){
+            std::cout << "\n job resumed" << std::endl;
+        }
+        else{
+            std::cout << "Failed to resume the jobs" <<std::endl;
+        }
+    }
+    else if(UserInput == "suspend" && pid != 0){
+        int suspendpid = 0;
+        suspendpid = suspendJobs(pid);
+        if(suspendpid == 0){
+            std::cout << "\n job suspended" << std::endl;
+        }
+        else{
+            std::cout << "Failed to suspend the jobs" <<std::endl;
+        }
+    }
+    else if(UserInput == "wait" && pid != 0){
+        int waitpid = 0;
+        waitpid = waitJobs(pid);
+        if(waitpid == 0){
+            std::cout << "\n job put on wait" << std::endl;
+        }
+        else{
+            std::cout << "Failed to put the jobs on hold" <<std::endl;
+        }
+    }
+    else if(UserInput == "exit" && (pid == '\n' || pid == 0)){
+        return 0;
     }
     /*int rc = fork();
     if(rc < 0){
